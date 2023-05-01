c12057001ada4fa312f95195dfd7c85db4557773
holalula <keepholo@gmail.com>
2023-05-01 10:50:23 +0800

    update ignore

--- test/test.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include <iostream>
-#include "../HaiBinQiangJia/include/ff/FFProcess.h"
-#include "../HaiBinQiangJia/include/utils/sigscanner.h"
-
-int main()
-{
-    std::cout << "HaiBinQiangJia\n";
-    FFProcess& fp = FFProcess::get_instance();
-}
